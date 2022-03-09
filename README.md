b130d7f13d6eb63e142542318ef212f9f52e52bb
Baptiste <baptiste.bucamp@epita.fr>
2022-03-09 12:19:45 +0100

    Add live badges

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # maven_training
+
+![example workflow](https://github.com/bapthub/maven_training/actions/workflows/build.yml/badge.svg)
+[![codecov](https://codecov.io/gh/bapthub/maven_training/branch/main/graph/badge.svg)](https://codecov.io/gh/bapthub/maven_training)
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -9,23 +9,30 @@ class SampleTest {
     Sample sample = new Sample();
 
     @Test
-    public void test_add() {
+    void test_add() {
         System.out.println("Inside testOP()");
         assertEquals(2, sample.op(ADD, 1,1));
     }
     @Test
-    public void test_mul() {
+    void test_mul() {
         System.out.println("Inside testOP()");
         assertEquals(4, sample.op(MULT, 2,2));
     }
     @Test
-    public void testFact3() {
+    void testFact3() {
         System.out.println("Inside testFact()");
         assertEquals(6, sample.fact(3));
     }
     @Test
-    public void testFact0() {
+    void testFact0() {
         System.out.println("Inside testFact()");
         assertEquals(1, sample.fact(0));
+    }
+    @Test
+    void fact_neg() {
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> {
+            sample.fact(-2);
+        });
+        assertEquals("N should be positive", exception.getMessage());
     }
 }
